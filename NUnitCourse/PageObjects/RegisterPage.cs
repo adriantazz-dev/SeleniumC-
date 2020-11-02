c5aa7ca75d43357958398838d82b0827be94c6aa
adriantazz <adriantazz@gmail.com>
2020-11-02 17:10:23 -0600

    test

---
+++ NUnitCourse/PageObjects/RegisterPage.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitCourse.PageObjects
+{
+    public class RegisterPage
+    {
+        IWebDriver driver;
+        //Constructor donde se le asigna el driver que recibimos como parametro a nuestro driver variable de clase
+        public RegisterPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        //Atributos
+        [FindsBy(How = How.CssSelector, Using = "input[name='name']")]
+        public IWebElement NameField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[name='email']")]
+        public IWebElement EmailField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[id='exampleInputPassword1']")]
+        public IWebElement PasswordField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[id='exampleCheck1']")]
+        public IWebElement IcecreamChecbox { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[name='bday']")]
+        public IWebElement DoBField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[value='Submit']")]
+        public IWebElement SubmitBtn { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div[class='alert alert-success alert-dismissible'] strong")]
+        public IWebElement SuccessMsg { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "select[id='exampleFormControlSelect1']")]
+        public IWebElement GenderDropdown { get; set; }
+
+        public void SelectGender(string Gender)
+        {
+            var GenderDropdown = driver.FindElement(By.CssSelector("select[id='exampleFormControlSelect1']"));
+            var selectElement = new SelectElement(GenderDropdown);
+            selectElement.SelectByText(Gender);
+        }
+
+        public void SelectStatus(string Status)
+        {   
+            if (Status == "Student")
+            {
+                driver.FindElement(By.CssSelector("input[id='inlineRadio1']")).Click();
+            }
+            else if (Status == "Employed")
+            {
+                driver.FindElement(By.CssSelector("input[id='inlineRadio2']")).Click();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ NUnitCourse/TestScripts/Module2.cs
@@ -0,0 +1,46 @@
+﻿using AventStack.ExtentReports;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using NUnitCourse.BaseClass;
+using NUnitCourse.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitCourse.TestScripts
+{  
+    [TestFixture]
+    public class TestRegisterPage : BaseTest
+    {   
+        [Test, Category("RegisterPage")]
+        public void TestRegistration()
+        {
+            //Se crea string para guardar el nombre del metodo para luego usarlo en el report
+            string testName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            //Se crea el objeto test
+            ExtentTest test = extent.CreateTest(testName).Info("Test started");
+
+            try
+            {
+                var RP = new RegisterPage(driver);
+                RP.NameField.SendKeys("Adrian");
+                RP.EmailField.SendKeys("Adrian@test.com");
+                RP.PasswordField.SendKeys("12345");
+                RP.IcecreamChecbox.Click();
+                RP.SelectGender("Male");
+                RP.SelectStatus("Student");
+                RP.DoBField.SendKeys("03/21/1992");
+                RP.SubmitBtn.Click();
+                var msg = RP.SuccessMsg.Text;
+                Assert.IsTrue(msg == "Success!");
+                test.Log(Status.Pass, testName + " Passed");
+            }
+            catch (Exception ex)
+            {
+
+                test.Log(Status.Error, "Se encontró un error: " + ex.Message);
+            }
+        }
+    }
+}
